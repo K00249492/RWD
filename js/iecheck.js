471a20f4e55fc62b7267fbbec530400f2b58ce91
K00249492 <k00249492@student.lit.ie>
2020-11-21 23:21:45 +0000

    Added the following webpages: article, login, register, profile, allnews, dummy; updated index

---
+++ js/iecheck.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", () => {
+function msieversion() {
+
+    var ua = window.navigator.userAgent;
+    var msie = ua.indexOf("MSIE ");
+
+    if (msie > 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./))  // If Internet Explorer, return version number
+    {
+        alert(parseInt(ua.substring(msie + 5, ua.indexOf(".", msie))));
+    }
+    else  // If another browser, return 0
+    {
+        alert('otherbrowser');
+    }
+
+    return false;
+}
+});
